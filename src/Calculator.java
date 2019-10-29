19af8d2184551ea53c428f63880a811775d3ae6a
Jake Patzer <37888505+jakepatzer@users.noreply.github.com>
2019-10-28 18:14:15 -0700

    Added user interface

---
+++ src/Calculator.java
@@ -0,0 +1,9 @@
+public class Calculator {
+
+    public Double calculate(String expr) {
+
+
+        return null;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+
+        Scanner scanner = new Scanner(System.in);
+        Calculator calculator = new Calculator();
+
+        System.out.println("Please enter an expression to evaluate, or \"q\" to quit\n");
+
+        while (true) {
+
+            String input = scanner.nextLine();
+
+            if (input.equals("q")) {
+                break;
+            }
+
+            Double result = calculator.calculate(input);
+
+            if (result == null) {
+                System.out.println("Please enter a valid expression\n");
+            }
+            else {
+                System.out.println(" = " + result.toString() + "\n");
+            }
+
+        }
+
     }
 
 }
